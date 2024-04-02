b7cb07dd59bcf3c61243a6d274b72d6083ef48cd
muntaha-194 <160383120+muntaha-194@users.noreply.github.com>
2024-04-02 15:19:19 +0500

    adding a number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\twelcome to Muntaha's - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 4 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess Number (Number limit from 1 to 4)",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you guess a correct number.");
+}
+else {
+    console.log("Sorry you guess a wrong Number");
+}
